8bb767e12c0e47867d0123265df301de7f003093
Richard Harding <rharding@mitechie.com>
2012-04-13 21:04:54 -0400

    No news here

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
-NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
 version = '0.1'
@@ -23,7 +22,7 @@ test_requires = [
 setup(name='bookie_api',
     version=version,
     description="Api and command line client for Bookie",
-    long_description=README + '\n\n' + NEWS,
+    long_description=README,
     classifiers=[
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
