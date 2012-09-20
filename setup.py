d765dd23cad1d283238fc7e35b74f2c1e7fe1ce2
Richard Harding <rharding@mitechie.com>
2012-09-19 20:37:46 -0400

    Update to v0.4.3

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 
-version = '0.4.2'
+version = '0.4.3'
 
 install_requires = [
     # List your project dependencies here.
